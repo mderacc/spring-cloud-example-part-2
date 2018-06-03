9e9176a2c5d102111508d0c07b2bc8eeaa33b27b
mderemer <external.z05mdere@decathlon.com>
2018-06-03 11:35:09 +0200

    feat(part2): add filter example

--- service-gateway/src/main/java/com/spring/cloud/servicegateway/GatewayServiceApplication.java
+++ service-gateway/src/main/java/com/spring/cloud/servicegateway/GatewayServiceApplication.java
@@ -1,10 +1,12 @@
 package com.spring.cloud.servicegateway;
 
+import com.spring.cloud.servicegateway.filters.pre.FilterExample;
 import java.net.MalformedURLException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,5 +14,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class GatewayServiceApplication {
 	public static void main(String[] args) throws MalformedURLException {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public FilterExample filterExample() {
+		return new FilterExample();
 	}
 }
---
+++ service-gateway/src/main/java/com/spring/cloud/servicegateway/filters/pre/FilterExample.java
@@ -0,0 +1,31 @@
+package com.spring.cloud.servicegateway.filters.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.Debug;
+import javax.servlet.http.HttpServletRequest;
+import com.netflix.zuul.context.RequestContext;
+
+public class FilterExample extends ZuulFilter {
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 10000;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return Debug.debugRequest();
+  }
+
+  @Override
+  public Object run() {
+    HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+    Debug.addRequestDebug("Requete:" + req.getScheme() + " Adresse:" + req.getRemoteAddr() + " Port:" + req.getRemotePort());
+    return null;
+  }
+}
